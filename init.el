d2698632098d9637c41de233e83ec1f709935a39
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2026-01-14 11:29:56 -0800

    feat: Enabled Alt+v page up.
    chore: Ignore unnecessary directories and files.

--- init.el
+++ init.el
@@ -1,332 +1,244 @@
-;;;;; Install packages
+
+;;; init.el --- Refactored Configuration
+;;;;; 1. Package Management Setup
+(require 'package)
 (setq package-archives
-  '(("gnu" . "https://elpa.gnu.org/packages/")
-    ("melpa" . "https://melpa.org/packages/")
-    ("org" . "https://orgmode.org/elpa/")))
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")))
+
 (package-initialize)
 
-(defvar my-package-list
-  '(
-     anzu
-     company
-     cmake-mode
-     dockerfile-mode
-     ini-mode
-     flycheck
-     js2-mode
-     json-mode
-     markdown-mode
-     multi-term
-     neotree
-     simple-httpd
-     stylus-mode
-     sws-mode
-     tide
-     typescript-mode
-     undo-fu
-     volatile-highlights
-     web-mode
-     yaml-mode)
-  "packages to be installed")
-(require 'package)
-(setq package-pinned-packages
-  '(
-     (anzu . "melpa")
-     (company . "melpa")
-     (cmake-mode . "melpa")
-     (dockerfile-mode . "melpa")
-     (flycheck . "melpa")
-     (ini-mode . "melpa")
-     (js2-mode . "melpa")
-     (json-mode . "melpa")
-     (markdown-mode . "melpa")
-     (multi-term . "melpa")
-     (neotree . "melpa")
-     (simple-httpd . "melpa")
-     (stylus-mode . "melpa")
-     (sws-mode . "melpa")
-     (tide . "melpa")
-     (typescript-mode . "melpa")
-     (undo-fu . "melpa")
-     (volatile-highlights . "melpa")
-     (web-mode . "melpa")
-     (yaml-mode . "melpa")))
+;; Install use-package if not present
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(let ((package-refreshed nil))
-  (dolist (pkg my-package-list)
-    (unless (package-installed-p pkg)
-      (when (null package-refreshed)
-        (package-refresh-contents)
-        (setq package-refreshed t))
-      (package-install pkg))))
+(eval-when-compile
+  (require 'use-package))
 
-;;;;; Character code
+;; Automatically install packages defined in use-package
+(setq use-package-always-ensure t)
+
+
+;;;;; 2. Basic Settings (Encoding, UI, Backup)
+
+;; Encoding
 (prefer-coding-system 'utf-8)
 (setq file-name-coding-system 'utf-8)
 (setq locale-coding-system 'utf-8)
+(set-locale-environment nil)
 
-;;;;; Basic config
+;; UI Cleanup
 (load-theme 'misterioso t)
 (column-number-mode t)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
+(setq inhibit-startup-message t) ; Hide splash screen
+
+;; File Handling
 (setq create-lockfiles nil)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq history-length 1024)
-(set-locale-environment nil) ;Don't use location-dependent setting
 
+;; Indentation Defaults
+(setq-default indent-tabs-mode nil) ; Use spaces
+(setq-default tab-width 2)
+(setq-default c-basic-offset 2)
+(setq indent-line-function 'insert-tab)
+
+;; Window Management
 (defun other-window-or-split ()
   (interactive)
   (when (one-window-p)
     (split-window-horizontally))
   (other-window 1))
-(define-key global-map (kbd "C-t") 'other-window-or-split)
-(define-key global-map (kbd "C-i") 'scroll-down-command)
 
-;; Leave copy/paste & modifyOtherKeys active, but skip reportBackground
-; (setq xterm-extra-capabilities
-;  '(modifyOtherKeys getSelection setSelection))
+(global-set-key (kbd "C-t") 'other-window-or-split)
+(global-set-key (kbd "C-i") 'scroll-down-command)
 
-(setq xterm-osc-52-max-size 10000)
-
-;;;;; Disable viewing help file
-(global-unset-key (kbd "C-h n")) ; NEWS
-(global-unset-key (kbd "C-h C-n")) ; NEWS
-(global-unset-key (kbd "C-h C-d")) ; How to debug/bug-report Emacs itself
-(global-unset-key (kbd "C-h C-c")) ; GPL copying conditions
-(global-unset-key (kbd "C-h C-e")) ; Where to get third party packages
-(global-unset-key (kbd "C-h C-f")) ; FAQ
-(global-unset-key (kbd "C-h g")) ; GNU Project
-(global-unset-key (kbd "C-h C-o")) ; How to download/get Emacs
-(global-unset-key (kbd "C-h C-p")) ; Known issues/work-arounds
-(global-unset-key (kbd "C-h C-t")) ; Emacs maintainers' to-do list
-(global-unset-key (kbd "C-h C-w")) ; Emacs "no warranty" notice
-(global-unset-key (kbd "C-h h")) ; HELLO file.
-(global-unset-key (kbd "C-h C-a")) ; About GNU Emacs
-(global-unset-key (kbd "<f1> a")) ; About GNU Emacs
-
-;;;;; Undo, redo
-(require 'undo-fu)
-
-(global-unset-key (kbd "C-z")) ; disable suspend button
-(global-unset-key (kbd "C-x C-z")) ; disable suspend button
-
-(global-set-key (kbd "C-z") 'undo-fu-only-undo)
-(global-set-key (kbd "C-/") 'undo-fu-only-redo)
-
-;;;;; Disable C-[ to close split window without disabling M-x.
-(defun my-keyboard-escape-quit ()
-  "An alternative to `keyboard-escape-quit' that doesn't close windows."
-  (interactive)
-  (let (orig-buf (current-buffer))
-    (dolist (buf (buffer-list))
-      (set-buffer buf)
-      (if (and buffer-file-name (buffer-modified-p))
-          (message "Modified file: %s" (buffer-file-name))
-        (when (and (get-buffer-process buf) (not (eq buf orig-buf)))
-          (set-process-query-on-exit-flag (get-buffer-process buf) nil)
-          (kill-buffer buf))))))
-
-(require 'anzu)
-(global-anzu-mode t)
-
-(require 'volatile-highlights)
-(volatile-highlights-mode t)
-
-;;;;; Auto-completion
-(require 'company)
-(global-company-mode)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 3)
-(setq company-selection-wrap-around t)
-(global-set-key (kbd "C-M-i") 'company-complete)
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-search-map (kbd "C-n") 'company-select-next)
-(define-key company-search-map (kbd "C-p") 'company-select-previous)
-(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-(define-key company-active-map (kbd "C-M-i") 'company-complete-selection)
-(define-key company-active-map [tab] 'company-complete-selection)
+;; Prevent "C-[" from accidentally closing windows via escape
 (define-key key-translation-map (kbd "C-]") (kbd "<escape>"))
-(define-key key-translation-map (kbd "C-[") nil)
 
-;;;;; Tab and whitespace
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq indent-line-function 'insert-tab)
-(setq c-basic-offset 2)
-(load "whitespace")
-(global-whitespace-mode t)
-(setq whitespace-style '(tab-mark))
-(setq-default show-trailing-whitespace t)
+;; Prevent "ESC ESC ESC" from closing windows.
+(global-set-key (kbd "ESC ESC ESC") 'keyboard-quit)
 
-;;;;; Bracket
-(setq show-paren-style 'mixed)
-(show-paren-mode t) ;Highlight a pair of bracket
-(electric-pair-mode t) ;Close bracket automatically
-(set-face-attribute 'show-paren-match nil :background "#5d5d5d")
+;; Disable annoying help bindings
+(dolist (key '("C-h n" "C-h C-n" "C-h C-d" "C-h C-c" "C-h C-e" "C-h C-f"
+               "C-h g" "C-h C-o" "C-h C-p" "C-h C-t" "C-h C-w" "C-h h"
+               "C-h C-a" "<f1> a"))
+  (global-unset-key (kbd key)))
 
-;;;;; Directory tree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-show-hidden-files t)
-(setq neo-smart-open t)
 
-;;;;; Python
-(setq-default python-indent 2)
-(setq-default python-indent-offset 2)
+;;;;; 3. Core Editor Features
 
-;;;;; C, C++
+;; Bracket Pair Handling
+(use-package elec-pair
+  :ensure nil ; Built-in
+  :hook (prog-mode . electric-pair-mode))
+
+(use-package paren
+  :ensure nil ; Built-in
+  :config
+  (setq show-paren-style 'mixed)
+  (show-paren-mode t)
+  (set-face-attribute 'show-paren-match nil :background "#5d5d5d"))
+
+;; Whitespace visualization
+(use-package whitespace
+  :ensure nil ; Built-in
+  :config
+  (setq whitespace-style '(tab-mark face)) ; Simple config
+  (setq-default show-trailing-whitespace t)
+  (global-whitespace-mode t))
+
+;; Undo/Redo (undo-fu)
+(use-package undo-fu
+  :config
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z") 'undo-fu-only-undo)
+  (global-set-key (kbd "C-/") 'undo-fu-only-redo))
+
+;; Visual feedback on operations
+(use-package volatile-highlights
+  :config
+  (volatile-highlights-mode t))
+
+;; Search/Replace visualizer
+(use-package anzu
+  :config
+  (global-anzu-mode t))
+
+;; Auto-completion (Company)
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2) ; Shortened slightly
+  (setq company-selection-wrap-around t)
+  (setq company-tooltip-align-annotations t)
+  :bind
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("C-s" . company-filter-candidates)
+        ("C-M-i" . company-complete-selection)
+        ("<tab>" . company-complete-selection))
+  (:map company-search-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous))
+  (:map global-map
+        ("C-M-i" . company-complete)))
+
+
+;;;;; 4. Navigation & Tools
+
+(use-package neotree
+  :bind ([f8] . neotree-toggle)
+  :config
+  (setq neo-show-hidden-files t)
+  (setq neo-smart-open t))
+
+(use-package multi-term
+  :config
+  (setq multi-term-program "/bin/bash")
+  (add-hook 'term-mode-hook
+            (lambda ()
+              (term-set-escape-char ?\C-t)
+              (define-key term-raw-map "\C-t" 'next-multiframe-window)
+              (define-key term-raw-map "\M-y" 'yank-pop)
+              (define-key term-raw-map "\M-w" 'kill-ring-save))))
+
+
+;;;;; 5. Programming Languages
+
+;; Syntax Checking
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+;; C/C++
 (add-to-list 'auto-mode-alist '("\\.cu?\\'" . c++-mode))
-(add-hook
-  'c-mode-common-hook
-  (lambda ()
-    (c-set-style "bsd")
-    (setq c-basic-offset 2)
-    (setq c-tab-always-indent t)
-    (setq tab-width 2)
-    (setq indent-tabs-mode nil) ; Use space for indent, not tab
-    (c-set-offset 'innamespace 2) ; Indent namespaces
-    (c-set-offset 'arglist-close 0)
-    (define-key c-mode-base-map "\C-m" 'newline-and-indent)
-  )
-)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (c-set-style "bsd")
+            (setq c-basic-offset 2)
+            (setq c-tab-always-indent t)
+            (setq indent-tabs-mode nil)
+            (c-set-offset 'innamespace 2)
+            (c-set-offset 'arglist-close 0)
+            ;; Enable newline-and-indent on Enter
+            (local-set-key "\C-m" 'newline-and-indent)))
 
-;;;;; Tex
-(setq auto-mode-alist
-  (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+;; Python
+(use-package python
+  :ensure nil ; Built-in
+  :config
+  (setq python-indent-offset 2))
 
-(setq tex-command "platex2pdf")
-(cond
-  ((eq system-type 'gnu/linux)
-    (setq dvi2-command "evince"))
-  ((eq system-type 'darwin)
-    (setq dvi2-command "open -a Preview")))
-(add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
+;; JSON / YAML / Docker / INI
+(use-package json-mode)
+(use-package yaml-mode)
+(use-package dockerfile-mode)
+(use-package ini-mode)
 
-;;;;; Web server
-;; Usage :
-;; 1. M-x customize-option
-;; 2. Customize variable: httpd-root
-;; 3. Set path to desired root dir.
-;; 4. M-x httpd-start
-;; 5. M-x httpd-stop
-(require 'simple-httpd)
-(setq httpd-port 8080)
+;; TypeScript
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :config
+  (setq typescript-indent-level 2))
 
-;;;;; HTML, CSS
-;; M-x package-list-packages
-;; package-install
-;; multi-web-mode
-;; If need to support template, M-x -> web-mode-set-engine -> jinja2
-;; web-modeの読み込みとファイル関連付け
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
+;; Web Mode (HTML, Vue, Templates)
+(use-package web-mode
+  :mode
+  ("\\.html?\\'" . web-mode)
+  ("\\.vue\\'"   . web-mode)
+  :config
+  (setq web-mode-enable-engine-detection t)
+  (setq web-mode-content-types-alist '(("vue" . "\\.vue\\'")))
 
-(setq web-mode-content-types-alist
-  '(("vue" . "\\.vue\\'")
-    ("typescript" . "\\.ts\\'")))
-
-(setq web-mode-enable-engine-detection t)
-
-(setq web-mode-indentation-params
-  '(("lineup-args" . nil)
-    ("lineup-calls" . nil)
-    ("lineup-concats" . nil)
-    ("lineup-ternary" . nil)))
-
-(defun my-web-mode-hook ()
-  "Personal `web-mode' customizations for HTML, Vue, etc."
+  ;; Indentation
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-script-padding 0)
   (setq web-mode-style-padding 0)
   (setq web-mode-block-padding 2)
-  (setq css-indent-level 2)
-  ;; Syntax highlighting toggles
+
+  ;; Highlighting & Behavior
   (setq web-mode-enable-current-element-highlight t)
-  (setq web-mode-enable-auto-pairing nil)
+  (setq web-mode-enable-auto-pairing nil) ; Let electric-pair handle it
   (setq web-mode-enable-auto-closing t)
   (setq web-mode-enable-css-colorization t)
-  ;; Face tweaks
+
+  ;; Faces (Colors)
   (set-face-attribute 'web-mode-comment-face nil :foreground "#6d6d6d")
   (set-face-attribute 'web-mode-doctype-face nil :foreground "Blue")
   (set-face-attribute 'web-mode-html-tag-face nil :foreground "#00cc66")
   (set-face-attribute 'web-mode-html-attr-value-face nil :foreground "#66cc00")
   (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#bdbdbd"))
 
-(add-hook 'web-mode-hook #'my-web-mode-hook)
+;; LaTeX (YaTeX)
+(use-package yatex
+  :ensure t
+  :mode ("\\.tex\\'" . yatex-mode)
+  :config
+  (setq tex-command "platex2pdf")
+  (setq dvi2-command (if (eq system-type 'darwin) "open -a Preview" "evince"))
+  (add-hook 'yatex-mode-hook (lambda () (setq auto-fill-function nil))))
 
-; (defun setup-tide-mode ()
-;   (interactive)
-;   (tide-setup)
-;   (flycheck-mode +1)
-;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;   (eldoc-mode +1)
-;   (tide-hl-identifier-mode +1)
-;   ;; configure javascript-tide checker to run after your default javascript checker
-;   (flycheck-add-next-checker 'typescript-tide 'javascript-eslint)
-;   (company-mode +1))
-;
-; (defun my-web-mode-ts-setup ()
-;   ;; Only run `setup-tide-mode` in .ts buffers
-;   (when (and buffer-file-name
-;              (string-equal "ts" (file-name-extension buffer-file-name)))
-;     (setup-tide-mode)))
-;
-; (add-hook 'web-mode-hook #'my-web-mode-ts-setup)
+;; Simple HTTPD (Kept but cleaned)
+(use-package simple-httpd
+  :config
+  (setq httpd-port 8080))
+  ;; Note: Set httpd-root manually via M-x customize-variable if needed,
+  ;; avoiding hardcoded paths in init.el for portability.
 
-;;;;; JavaScript
-(setq js-indent-level 2)
-
-;;;;; Json
-(require 'json-mode)
-(add-to-list 'auto-mode-alist '("\\.json?\\'" . json-mode))
-
-;;;;; Yaml
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yaml?\\'" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yml?\\'" . yaml-mode))
-
-;;;;; INI
-(require 'ini-mode)
-(add-to-list 'auto-mode-alist '("\\.ini?\\'" . ini-mode))
-(add-to-list 'auto-mode-alist '("\\.in?\\'" . ini-mode))
-
-;;;;; Dockerfile
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("\\Dockerfile\\'" . dockerfile-mode))
-
-;;;;; Terminal
-(require 'multi-term)
-(setq multi-term-program "/bin/bash")
-
-(add-hook 'term-mode-hook
-  (lambda ()
-    (term-set-escape-char ?\C-t)
-    (define-key term-raw-map "\C-t" 'next-multiframe-window)
-    (define-key term-raw-map "\M-y" 'yank-pop)
-    (define-key term-raw-map "\M-w" 'kill-ring-save)))
-
-(add-hook 'term-mode-hook
-  (lambda ()
-    (term-set-escape-char ?\C-t)
-    (define-key term-raw-map "\C-t" 'next-multiframe-window)
-    (define-key term-raw-map "\M-y" 'yank-pop)
-    (define-key term-raw-map "\M-w" 'kill-ring-save)))
-
-;;;;; The follows are automatically generated.
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(httpd-root "~/Work/genki/app")
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -335,32 +247,28 @@
  ;; If there is more than one, they won't work right.
  )
 
-;;;;; Typescript
-; (require 'typescript-mode)
-; (add-to-list 'auto-mode-alist '("\\.ts?\\'" . typescript-mode))
+ ;; Rust
+(use-package rust-mode
+  :mode "\\.rs\\'"
+  :config
+  (setq rust-format-on-save t)) ; Auto-format on save
 
-; (defun setup-tide-mode ()
-;   (interactive)
-;   (tide-setup)
-;   (flycheck-mode +1)
-;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;   (eldoc-mode +1)
-;   (tide-hl-identifier-mode +1)
-;   ;; configure javascript-tide checker to run after your default javascript checker
-;   (flycheck-add-next-checker 'typescript-tide 'javascript-eslint)
-;   (company-mode +1))
+;; Cargo Integration
+(use-package cargo
+  :hook (rust-mode . cargo-minor-mode))
 
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
+;; LSP (Language Server Protocol)
+;; Core LSP features: completion, defintions, and errors
+(use-package lsp-mode
+  :init
+  ;; Performance tunning (Recommended settings)
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-idle-delay 0.500)
+  :hook
+  ((rust-mode . lsp)
+   (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
-;; formats the buffer before saving
-; (add-hook 'before-save-hook 'tide-format-before-save)
-
-; (add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;; Emacs lisp
-(add-hook 'emacs-lisp-mode-hook
-  (lambda ()
-    (setq lisp-indent-offset 2)
-    (setq tab-width 2)
-    (setq indent-tabs-mode nil)))
+;; LSP UI (popups, documentation, etc.)
+(use-package lsp-ui
+  :commands lsp-ui-mode)
